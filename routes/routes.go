30fafef46ffd5697db1968cd63d5cf12abed88e1
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-06-17 11:45:42 +0530

    Refactor(routes):instead of grouping the home route response in to the /api romve from that group and add to the app.

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,26 @@ func SetupRoutes(app *fiber.App, db *mongo.Database) {
     controllers.InitUserController(db)
     controllers.InitBookController(db)
 
+
+    //Home route
+    app.Get("/", func(c *fiber.Ctx) error {
+    htmlContent := `
+        <html>
+            <head>
+                <title>CollectHub API</title>
+            </head>
+            <body>
+                <h1>🚀 Welcome to the CollectHub API</h1>
+                <p>This is the backend service for managing users and book collections.</p>
+            </body>
+        </html>
+    `
+     return c.Type("html").SendString(htmlContent)
+    })
+
     api := app.Group("/api")
+
+
 
     // User Routes
     api.Post("/users", controllers.CreateUser)
