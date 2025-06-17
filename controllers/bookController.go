28c9a0e3252a97475371a59c0521c46493f87ecb
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-06-17 10:51:58 +0530

    Feat(controllers):write book & user controllers.

---
+++ controllers/bookController.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+    "context"
+    "go-fiber-mongo-api/models"
+    "time"
+
+    "github.com/gofiber/fiber/v2"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    "go.mongodb.org/mongo-driver/mongo"
+)
+
+var bookCollection *mongo.Collection
+
+func InitBookController(db *mongo.Database) {
+    bookCollection = db.Collection("books")
+}
+
+func CreateBook(c *fiber.Ctx) error {
+    var book models.Book
+    if err := c.BodyParser(&book); err != nil {
+        return c.Status(400).JSON(fiber.Map{"error": "cannot parse JSON"})
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    res, err := bookCollection.InsertOne(ctx, book)
+    if err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "failed to insert book"})
+    }
+
+    return c.JSON(res)
+}
+
+func GetBooksByUser(c *fiber.Ctx) error {
+    userID := c.Params("userId")
+    objID, err := primitive.ObjectIDFromHex(userID)
+    if err != nil {
+        return c.Status(400).JSON(fiber.Map{"error": "invalid user ID"})
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    filter := bson.M{"user_id": objID}
+    cursor, err := bookCollection.Find(ctx, filter)
+    if err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "failed to fetch books"})
+    }
+
+    var books []models.Book
+    if err = cursor.All(ctx, &books); err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "error decoding books"})
+    }
+
+    return c.JSON(books)
+}
---
+++ controllers/userController.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+    "context"
+    "go-fiber-mongo-api/models"
+    "log"
+    "time"
+
+    "github.com/gofiber/fiber/v2"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection
+
+func InitUserController(db *mongo.Database) {
+    userCollection = db.Collection("users")
+}
+
+func CreateUser(c *fiber.Ctx) error {
+    var user models.User
+    if err := c.BodyParser(&user); err != nil {
+        return c.Status(400).JSON(fiber.Map{"error": "cannot parse JSON"})
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    res, err := userCollection.InsertOne(ctx, user)
+    if err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "failed to insert user"})
+    }
+
+    return c.JSON(res)
+}
+
+func GetUsers(c *fiber.Ctx) error {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    cursor, err := userCollection.Find(ctx, bson.M{})
+    if err != nil {
+        return c.Status(500).JSON(fiber.Map{"error": "failed to fetch users"})
+    }
+    var users []models.User
+    if err = cursor.All(ctx, &users); err != nil {
+        log.Fatal(err)
+    }
+
+    return c.JSON(users)
+}
